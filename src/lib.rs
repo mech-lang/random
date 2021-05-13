e134be187dbde73e5a1253f117b57cfc9c6a57a9
cmontella <cmontella@live.com>
2021-05-13 19:13:37 -0400

    fix random

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ extern crate oorandom;
 use mech_core::{Transaction};
 use mech_core::{Value, ValueMethods, IndexIterator, Table, ValueIterator};
 use mech_core::{Quantity, ToQuantity, QuantityMath, hash_string};
-use oorandom::Rand64;
+use oorandom::Rand32;
 use std::sync::Mutex;
 
 static DEFAULT_SEED: u64 = 0x242689ed11c79076;
@@ -14,7 +14,7 @@ static DEFAULT_SEED: u64 = 0x242689ed11c79076;
 lazy_static! {
   static ref ROWS: u64 = hash_string("rows");
   static ref COLUMNS: u64 = hash_string("columns");
-  static ref RNG: Mutex<Rand64> = Mutex::new(Rand64::new(DEFAULT_SEED.into()));
+  static ref RNG: Mutex<Rand32> = Mutex::new(Rand32::new(DEFAULT_SEED.into()));
 }
 
 #[no_mangle]
@@ -30,7 +30,7 @@ pub extern "C" fn random_uniform(arguments: &Vec<(u64, ValueIterator)>) {
           let mut rng = RNG.lock().unwrap();
           for out_ix in out.linear_index_iterator() {
             let mut random_value = rng.rand_float();
-            out.set_unchecked_linear(out_ix, Value::from_f64(random_value));
+            out.set_unchecked_linear(out_ix, Value::from_f32(random_value));
           }
         }
         _ => (),
@@ -53,8 +53,8 @@ pub extern "C" fn random_uniform__integer(arguments: &Vec<(u64, ValueIterator)>)
           out.resize(rows_u64 as usize, columns_u64 as usize);
           let mut rng = RNG.lock().unwrap();
           for out_ix in out.linear_index_iterator() {
-            let mut random_value = rng.rand_u64();
-            out.set_unchecked_linear(out_ix, Value::from_u64(random_value));
+            let mut random_value = rng.rand_u32();
+            out.set_unchecked_linear(out_ix, Value::from_u32(random_value));
           }
         }
         _ => (),
